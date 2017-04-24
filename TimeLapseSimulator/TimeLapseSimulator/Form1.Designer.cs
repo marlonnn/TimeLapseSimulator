8863339895f760833123c327973833d0d1a641e2
marlonnn <zhongw_1988@126.com>
2017-04-24 16:41:30 +0800

    添加Plate自定义控件

--- TimeLapseSimulator/TimeLapseSimulator/Form1.Designer.cs
+++ TimeLapseSimulator/TimeLapseSimulator/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.plate1 = new TimeLapseSimulator.UI.Plate();
+            this.plate2 = new TimeLapseSimulator.UI.Plate();
+            this.plate3 = new TimeLapseSimulator.UI.Plate();
+            this.SuspendLayout();
+            // 
+            // plate1
+            // 
+            this.plate1.CellsFontSize = 8F;
+            this.plate1.ColumHeaderFontSize = 9F;
+            this.plate1.Location = new System.Drawing.Point(56, 51);
+            this.plate1.Name = "plate1";
+            this.plate1.PlateColumnHeadersHeight = 25;
+            this.plate1.PlateColumns = 3;
+            this.plate1.PlateRowHeadersWidth = 25;
+            this.plate1.PlateRows = 5;
+            this.plate1.PlateWellsHeight = 25;
+            this.plate1.PlateWellsWidth = 25;
+            this.plate1.RowHeaderFontSize = 9F;
+            this.plate1.Size = new System.Drawing.Size(102, 152);
+            this.plate1.TabIndex = 0;
+            // 
+            // plate2
+            // 
+            this.plate2.CellsFontSize = 8F;
+            this.plate2.ColumHeaderFontSize = 9F;
+            this.plate2.Location = new System.Drawing.Point(201, 51);
+            this.plate2.Name = "plate2";
+            this.plate2.PlateColumnHeadersHeight = 25;
+            this.plate2.PlateColumns = 3;
+            this.plate2.PlateRowHeadersWidth = 25;
+            this.plate2.PlateRows = 5;
+            this.plate2.PlateWellsHeight = 25;
+            this.plate2.PlateWellsWidth = 25;
+            this.plate2.RowHeaderFontSize = 9F;
+            this.plate2.Size = new System.Drawing.Size(102, 152);
+            this.plate2.TabIndex = 1;
+            // 
+            // plate3
+            // 
+            this.plate3.CellsFontSize = 8F;
+            this.plate3.ColumHeaderFontSize = 9F;
+            this.plate3.Location = new System.Drawing.Point(348, 51);
+            this.plate3.Name = "plate3";
+            this.plate3.PlateColumnHeadersHeight = 25;
+            this.plate3.PlateColumns = 3;
+            this.plate3.PlateRowHeadersWidth = 25;
+            this.plate3.PlateRows = 5;
+            this.plate3.PlateWellsHeight = 25;
+            this.plate3.PlateWellsWidth = 25;
+            this.plate3.RowHeaderFontSize = 9F;
+            this.plate3.Size = new System.Drawing.Size(102, 152);
+            this.plate3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 262);
+            this.Controls.Add(this.plate3);
+            this.Controls.Add(this.plate2);
+            this.Controls.Add(this.plate1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private UI.Plate plate1;
+        private UI.Plate plate2;
+        private UI.Plate plate3;
     }
 }
 
---
+++ TimeLapseSimulator/TimeLapseSimulator/UI/Plate.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeLapseSimulator.UI
+{
+    public partial class Plate : UserControl
+    {
+        private sealed class WellValue
+        {
+            private Color _color;
+            public Color BackColor
+            {
+                set
+                {
+                    _color = value;
+                }
+                get
+                {
+                    return _color;
+                }
+            }
+
+            private Color _fontColor = Color.Black;
+            public Color FontColor
+            {
+                set
+                {
+                    _fontColor = value;
+                }
+                get
+                {
+                    return _fontColor;
+                }
+            }
+        }
+
+        private DataGridView grid;
+
+        private int plateRows = 5;
+        private int plateColumns = 3;
+        private int plateWellsWidth = 25;
+        private int plateWellsHeight = 25;
+        private int plateColumnHeadersHeight = 25;
+        private int plateRowHeadersWidth = 25;
+        private float rowHeaderFontSize = 9;
+        private float columHeaderFontSize = 9;
+        private float cellsFontSize = 8;
+
+        WellValue[,] wellvalues;
+
+        public int PlateRows
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Can not set Plate Rows to zero !");
+                }
+                plateRows = value;
+                InitialPlate();
+            }
+            get
+            {
+                return plateRows;
+            }
+        }
+        public int PlateColumns
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Can not set Plate Columns to zero !");
+                }
+                plateColumns = value;
+                InitialPlate();
+            }
+            get
+            {
+                return plateColumns;
+            }
+        }
+
+        public int PlateWellsWidth
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Can not set PlateWellsWidth to zero !");
+                }
+                plateWellsWidth = value;
+                InitialPlate();
+            }
+            get
+            {
+                return plateWellsWidth;
+            }
+        }
+        public int PlateWellsHeight
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Can not set Plate Wells Height to zero !");
+                }
+                plateWellsHeight = value;
+                InitialPlate();
+            }
+            get
+            {
+                return plateWellsHeight;
+            }
+        }
+
+        public int PlateColumnHeadersHeight
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Can not set PlateColumnHeadersHeight to zero !");
+                }
+                plateColumnHeadersHeight = value;
+                InitialPlate();
+            }
+            get
+            {
+                return plateColumnHeadersHeight;
+            }
+        }
+        public int PlateRowHeadersWidth
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Can not set PlateColumnHeadersHeight to zero !");
+                }
+                plateRowHeadersWidth = value;
+                InitialPlate();
+            }
+            get
+            {
+                return plateRowHeadersWidth;
+            }
+        }
+
+        public float RowHeaderFontSize
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("The number must beyond 0 ! ");
+                }
+                rowHeaderFontSize = value;
+                InitialPlate();
+            }
+            get
+            {
+                return rowHeaderFontSize;
+            }
+        }
+        public float ColumHeaderFontSize
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("The number must beyond 0 ! ");
+                }
+                columHeaderFontSize = value;
+                InitialPlate();
+            }
+            get
+            {
+                return columHeaderFontSize;
+            }
+        }
+        public float CellsFontSize
+        {
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("The number must beyond 0 ! ");
+                }
+                cellsFontSize = value;
+                InitialPlate();
+            }
+            get
+            {
+                return cellsFontSize;
+            }
+        }
+
+        private void InitialPlate()
+        {
+            if (this.grid == null) return;
+            this.grid.Rows.Clear();
+            this.grid.Columns.Clear();
+            SetWells();
+            SetFontSize();
+            SetWellsValue();
+        }
+
+        private void SetPlate()
+        {
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToResizeColumns = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.grid.ReadOnly = true;
+            this.grid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.grid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.grid.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.grid.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            this.grid.GridColor = System.Drawing.Color.DarkGray;
+            this.grid.BorderStyle = BorderStyle.FixedSingle;
+            this.grid.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            this.grid.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            this.grid.EnableHeadersVisualStyles = false;
+            this.grid.RowHeadersDefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(200, 214, 231);
+            this.grid.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(200, 214, 231);
+            this.grid.RowHeadersDefaultCellStyle.Padding = new Padding(this.grid.RowHeadersWidth);
+        }
+
+        private void SetWellsValue()
+        {
+            //_rightClickWell = new WellID();
+            //_rightClickWell.InitialWellID();
+            wellvalues = new WellValue[PlateRows, PlateColumns];
+            for (int i = 0; i < wellvalues.GetLength(0); i++)
+            {
+                for (int j = 0; j < wellvalues.GetLength(1); j++)
+                {
+                    wellvalues[i, j] = new WellValue();
+                }
+            }
+
+            //MultiSelectChanged();
+        }
+
+        private void SetFontSize()
+        {
+            this.grid.RowHeadersDefaultCellStyle.Font = new Font("Arial", RowHeaderFontSize);
+            this.grid.ColumnHeadersDefaultCellStyle.Font = new Font("Arial", ColumHeaderFontSize);
+            this.grid.DefaultCellStyle.Font = new Font("Arial", CellsFontSize);
+        }
+
+        private void SetWells()
+        {
+            for (int i = 1; i <= PlateColumns; i++)
+            {
+                DataGridViewImageColumn imgcol = new DataGridViewImageColumn();
+                imgcol.HeaderText = i.ToString();
+                imgcol.Width = PlateWellsWidth;
+                this.grid.Columns.Add(imgcol);
+            }
+            System.Drawing.Image image = new System.Drawing.Bitmap(PlateWellsWidth, PlateWellsHeight);
+            using (Graphics graphhic = System.Drawing.Graphics.FromImage(image))
+            {
+                using (Brush brush = new SolidBrush(Color.White))
+                {
+                    graphhic.FillRectangle(brush, new Rectangle(0, 0, image.Width, image.Height));
+                }
+            }
+            for (int i = 0; i < PlateRows; i++)
+            {
+                this.grid.Rows.Add();
+                this.grid.Rows[i].Height = PlateWellsHeight;
+                for (int j = 0; j < PlateColumns; j++)
+                {
+                    this.grid[j, i].Value = image;
+                }
+            }
+            this.grid.ColumnHeadersHeight = PlateColumnHeadersHeight;
+            this.grid.RowHeadersWidth = PlateRowHeadersWidth;
+        }
+
+        public Plate()
+        {
+            InitializeComponent();
+            this.Load += Plate_Load;
+        }
+
+        private void Plate_Load(object sender, EventArgs e)
+        {
+            NewAPlate();
+            InitialPlate();
+        }
+
+        private void NewAPlate()
+        {
+            this.grid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.Location = new System.Drawing.Point(0, 0);
+            this.grid.Name = "Plate";
+            SetPlate();
+            this.grid.TabIndex = 0;
+            this.grid.RowPostPaint += new DataGridViewRowPostPaintEventHandler(Plate_RowPostPaint);
+            //this.grid.CellStateChanged += new DataGridViewCellStateChangedEventHandler(Plate_CellStateChanged);
+            //this.grid.RowHeaderMouseClick += new DataGridViewCellMouseEventHandler(Plate_RowHeaderMouseClick);
+            //this.grid.ColumnHeaderMouseClick += new DataGridViewCellMouseEventHandler(Plate_ColumnHeaderMouseClick);
+            //this.grid.CellMouseClick += new DataGridViewCellMouseEventHandler(Plate_CellMouseClick);
+            //this.grid.MouseDown += new MouseEventHandler(Plate_MouseDown);
+            //this.grid.CurrentCellChanged += new EventHandler(Plate_CurrentCellChanged);
+            //this.grid.CellDoubleClick += new DataGridViewCellEventHandler(Plate_CellDoubleClick);
+            this.Controls.Add(this.grid);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private void Plate_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            Rectangle rectangle = new Rectangle(e.RowBounds.Location.X,
+              Convert.ToInt32(e.RowBounds.Location.Y + (e.RowBounds.Height - this.grid.RowHeadersDefaultCellStyle.Font.Size) / 2),
+              this.grid.RowHeadersWidth - 4, e.RowBounds.Height);
+            TextRenderer.DrawText(e.Graphics, string.Format("{0}", (char)('A' + e.RowIndex)), this.grid.RowHeadersDefaultCellStyle.Font,
+                rectangle, this.grid.RowHeadersDefaultCellStyle.ForeColor, TextFormatFlags.Right);
+        }
+
+        //画背景色
+        private void DrawColor(DataGridViewCell dvc)
+        {
+            Image image = new Bitmap(dvc.Value as Image);
+            if (image == null) return;
+            using (Graphics graphhic = System.Drawing.Graphics.FromImage(image))
+            {
+                using (Brush brush = new SolidBrush(wellvalues[dvc.RowIndex, dvc.ColumnIndex].BackColor))
+                {
+                    if (wellvalues[dvc.RowIndex, dvc.ColumnIndex].BackColor == Color.Gray
+                        || wellvalues[dvc.RowIndex, dvc.ColumnIndex].BackColor == Color.White)
+                    {
+                        graphhic.FillRectangle(brush, new Rectangle(0, 0, image.Width, image.Height));
+                    }
+                    else
+                    {
+                        graphhic.FillRectangle(brush, new Rectangle(2, 2, image.Width - 5, image.Height - 5));
+                    }
+                    dvc.Value = (System.Drawing.Bitmap)(image);
+                }
+            }
+        }
+    }
+}
